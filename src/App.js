123c9b2b1eadc48cdc495a2bf2256ff98e7602e7
karim chebbi <karimchebbion@gmail.com>
2024-10-22 10:56:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Todo from './pages/Todo';
+import Contact from './pages/Contact';
+import ErrorPage from './pages/ErrorPage';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-700">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo-app" element={<Todo />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
